a03ac790f88a420be9e6a0465c698aa6277250d0
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-16 21:54:19 +0530

    Code Updated

---
+++ src/routes/(adminLayout)/admin/dashboard/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { supabase } from '$lib/supabaseClient';
+
+	const logout = async () => {
+		let { error } = await supabase.auth.signOut();
+		if (!error) {
+			goto('/admin/login');
+		}
+	};
+</script>
+
+<header class="navbar bg-base-100">
+	<div class="flex-1">
+		<a href="/admin/dashboard" class="btn btn-ghost text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none gap-2">
+		<button type="button" class=" btn btn-error font-bold uppercase" on:click={logout}
+			>Logout</button
+		>
+	</div>
+</header>
