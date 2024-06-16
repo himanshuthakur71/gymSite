a3606107e89c0d6f7218426b79ab6105f1a86dc0
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-16 14:12:22 +0530

    Code Updated

--- src/routes/(publicLayout)/why-us/+page.svelte
+++ src/routes/(publicLayout)/why-us/+page.svelte
@@ -31,3 +31,75 @@
 		</div>
 	</div>
 </section>
+
+<section class="w-full bg-base-100 py-32" data-theme="fantasy">
+	<div class="hms-container">
+		<h2
+			class="relative mb-8 flex justify-center pb-8 text-3xl font-black after:absolute after:bottom-0 after:block after:h-[10px] after:w-[70px] after:bg-primary md:text-4xl lg:text-5xl"
+		>
+			Our Core Values
+		</h2>
+		<div class="w-full">
+			<ol class=" list-decimal marker:text-3xl marker:font-black marker:text-primary">
+				<li>
+					<h3 class=" mb-2 text-xl font-black uppercase text-primary md:text-2xl lg:text-3xl">
+						“WE before Me”
+					</h3>
+					<p class="text-lg md:text-xl lg:text-2xl">
+						FITNESS ACADEMY is a team effort. We operate in a transparent, professional, but also
+						high energy and fun environment where everyone has a voice that matters. We are selfless
+						in our approach. We invest in our team members so that we can fully invest in our
+						clients.
+					</p>
+				</li>
+				<li class="mt-4">
+					<h3 class=" mb-2 text-xl font-black uppercase text-primary md:text-2xl lg:text-3xl">
+						Unparalleled Dedication
+					</h3>
+					<p class="text-lg md:text-xl lg:text-2xl">
+						Every day is game day at FITNESS ACADEMY. We are on time, focused, efficient, and
+						energized for every client, every day.
+					</p>
+				</li>
+
+				<li class="mt-4">
+					<h3 class=" mb-2 text-xl font-black uppercase text-primary md:text-2xl lg:text-3xl">
+						Proactive
+					</h3>
+					<p class="text-lg md:text-xl lg:text-2xl">
+						We practice what we preach. We do our own workouts in order to experience what the
+						clients are going through. We push and encourage members to progress so that they can
+						see results.
+					</p>
+				</li>
+
+				<li class="mt-4">
+					<h3 class=" mb-2 text-xl font-black uppercase text-primary md:text-2xl lg:text-3xl">
+						Passionate
+					</h3>
+					<p class="text-lg md:text-xl lg:text-2xl">
+						We are not just another gym. We are a supportive community for our clients with the same
+						long-term goal for everyone: “live life to the fullest.” We believe in our services and
+						want for our clients what we want for ourselves.
+					</p>
+				</li>
+
+				<li class="mt-4">
+					<h3 class=" mb-2 text-xl font-black uppercase text-primary md:text-2xl lg:text-3xl">
+						Evolving
+					</h3>
+					<p class="text-lg md:text-xl lg:text-2xl">
+						We yearn to learn. This means constantly improving ourselves, our services, and the
+						lives of our clients. We never remain static, knowing all too well that training and
+						performance is a dynamic industry. We stay on top of new research, trends and science.
+						We embrace change and will always drive towards excellence.
+					</p>
+				</li>
+			</ol>
+		</div>
+
+		<div class=" mt-16">
+			<a href="/blog" class="btn btn-accent btn-block max-w-[120px] text-xl font-[600]">BACK</a>
+		</div>
+	</div>
+</section>
