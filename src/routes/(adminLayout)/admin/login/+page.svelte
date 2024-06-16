3aaa527e2d4ee169b24e6caf7422e4c4f12615f4
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-16 20:39:08 +0530

    Superbase added

---
+++ src/routes/(adminLayout)/admin/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { supabase } from '$lib/supabaseClient';
+
+	const user_signup = async (email: string, password: string) => {
+		let { data, error } = await supabase.auth.signUp({
+			email: email,
+			password: password
+		});
+
+		console.log({
+			data,
+			error
+		});
+	};
+</script>
+
+<!-- <button
+	class="btn btn-primary"
+	type="button"
+	on:click={() => user_signup('himanshupundir71@gmail.com', 'password@fitnessacademygym')}
+	>Create User</button
+> -->
