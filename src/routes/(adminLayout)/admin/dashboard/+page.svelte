380708fb55eb7243ae009417ef91bd2974e7b0df
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-16 22:10:49 +0530

    Code Updated

---
+++ src/routes/(adminLayout)/admin/dashboard/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { userStore } from '$lib/Stores/userStore';
+</script>
+
+<section>
+	<div class="hms-container">
+		<div class="w-full">
+			<div class="my-16">
+				<h1 class=" text-2xl lg:text-3xl">
+					Welcome, <strong>{$userStore?.user_metadata?.first_name}</strong>
+				</h1>
+			</div>
+		</div>
+	</div>
+</section>
