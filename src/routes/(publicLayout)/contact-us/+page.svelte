588112adf6c0d385eb0888d132c634fd582a19e9
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-16 15:01:20 +0530

    Code Updated

--- src/routes/(publicLayout)/contact-us/+page.svelte
+++ src/routes/(publicLayout)/contact-us/+page.svelte
@@ -32,7 +32,7 @@
 				<div class="w-full">
 					<iframe
 						title="google map location"
-						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d415.33466843698875!2d77.30123684994595!3d28.96197634890874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c519af625ea05%3A0x42ff0da83f71a536!2sMeetli%2C%20Uttar%20Pradesh%20250601!5e1!3m2!1sen!2sin!4v1718529376405!5m2!1sen!2sin"
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3322.6659937877957!2d77.3022743!3d28.9623301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390c511d08b98719%3A0x4708af98fd71cd6d!2sFitness%20academy!5e1!3m2!1sen!2sin!4v1718529926255!5m2!1sen!2sin"
 						width="100%"
 						height="250"
 						style="border:0;"
@@ -49,7 +49,7 @@
 							<li>
 								<a
 									href="tel:+919058884985"
-									class="flex items-center gap-1 text-lg hover:link-primary"
+									class="flex items-center gap-4 text-lg hover:link-primary"
 								>
 									<figure class=" flex items-center justify-center rounded-full bg-accent p-2">
 										<svg
@@ -68,7 +68,36 @@
 								</a>
 							</li>
 							<li class="mt-2">
-								<p class="flex items-center gap-1">
+								<div class="flex items-start gap-4">
+									<div>
+										<figure class=" flex items-center justify-center rounded-full bg-accent p-2">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												width="20px"
+												height="20px"
+												viewBox="0 0 24 24"
+											>
+												<g fill="none">
+													<path
+														d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+													/>
+													<path
+														fill="currentColor"
+														d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2m0 4a1 1 0 0 0-1 1v5a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V7a1 1 0 0 0-1-1"
+													/>
+												</g>
+											</svg>
+										</figure>
+									</div>
+									<p class="grid">
+										<span><b>Morning</b> 5am to 11am Male </span>
+										<span><b>Afternoon</b> 11am to 4pm Female </span>
+										<span><b>Evening</b> 4:30pm to 10pm Male</span>
+									</p>
+								</div>
+							</li>
+							<li class="mt-2">
+								<p class="flex items-center gap-4">
 									<figure class=" flex items-center justify-center rounded-full bg-accent p-2">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
