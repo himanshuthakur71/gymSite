3ef3f94ee000f93e467c3170f1abb7414fabf9d2
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-17 16:48:51 +0530

    Code Updated

--- src/routes/(publicLayout)/gallery/+page.svelte
+++ src/routes/(publicLayout)/gallery/+page.svelte
@@ -1,5 +1,8 @@
-<script>
+<script lang="ts">
 	import Header from '$lib/Components/Header.svelte';
+
+	export let data;
+	const { galleryImages } = data;
 </script>
 
 <svelte:head>
@@ -14,8 +17,29 @@
 	<div class="flex min-h-[calc(100vh-195px)] w-full items-center">
 		<div class="flex w-full flex-col items-end gap-8 bg-[rgba(0,0,0,0.6)] px-[15px] py-4">
 			<h1 class=" w-full border-primary py-1 text-center text-4xl font-black md:text-5xl">
-				PHOTOS <span class="text-primary">GALLERY</span>
+				PHOTOS & VIDEO <span class="text-primary">GALLERY</span>
 			</h1>
 		</div>
 	</div>
 </div>
+
+<section class="w-full bg-base-100 py-32" data-theme="fantasy">
+	<div class="hms-container">
+		{#if galleryImages?.length}
+			<div class="w-full">
+				<h2 class="text-xl font-semibold md:text-2xl lg:text-3xl">Photo Gallery</h2>
+				<div class="mt-2 w-full">
+					<div class="grid grid-cols-1 place-items-center gap-3 p-4 md:grid-cols-2 lg:grid-cols-3">
+						{#each galleryImages as item, i}
+							<img
+								class="hover:opacity-75"
+								src={`https://itkrfzkrbqgsscxcveae.supabase.co/storage/v1/object/public/gallery/images/${item?.name}`}
+								alt="gallery image {i}"
+							/>
+						{/each}
+					</div>
+				</div>
+			</div>
+		{/if}
+	</div>
+</section>
