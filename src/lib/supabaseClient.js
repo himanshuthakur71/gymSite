eab7c837a5bd754b89cddeb69122d2f0bbe0cbcb
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-17 08:42:44 +0530

    code Updated

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,7 +4,7 @@ const SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
 
 const SUPABASE_ANON_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log('Supabase URL:', SUPABASE_URL);
-console.log('Supabase API Key:', SUPABASE_ANON_KEY);
+// console.log('Supabase URL:', SUPABASE_URL);
+// console.log('Supabase API Key:', SUPABASE_ANON_KEY);
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
