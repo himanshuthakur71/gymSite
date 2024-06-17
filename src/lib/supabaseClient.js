68d47a7945d64033bd286fad1b4f2cbb85762b9a
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-17 14:12:08 +0530

    env varible added

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+// console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+// console.log('Supabase API Key:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 
-const SUPABASE_ANON_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
-
-// console.log('Supabase URL:', SUPABASE_URL);
-// console.log('Supabase API Key:', SUPABASE_ANON_KEY);
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY
+);
