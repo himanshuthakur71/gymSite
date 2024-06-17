2bd333064ed1fb634640d24ed24021dd406640ea
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-17 17:01:38 +0530

    Code Updated

--- src/routes/(adminLayout)/admin/dashboard/gallery/+layout.ts
+++ src/routes/(adminLayout)/admin/dashboard/gallery/+layout.ts
@@ -19,7 +19,24 @@ export const load: LayoutLoad = async () => {
 		return data;
 	}
 
+	async function getAllFilesInVideoFolder() {
+		// Retrieve the list of files in the 'images' folder of the 'gallery' bucket
+		const { data, error } = await supabase.storage.from('gallery').list('video', {
+			limit: 100, // Adjust the limit as needed
+			offset: 0,
+			sortBy: { column: 'name', order: 'asc' } // Optional: Sort files by name
+		});
+
+		if (error) {
+			console.error('Error retrieving files:', error);
+			return [];
+		}
+
+		return data;
+	}
+
 	return {
-		galleryImages: await getAllFilesInImagesFolder()
+		galleryImages: await getAllFilesInImagesFolder(),
+		galleryVideos: await getAllFilesInVideoFolder()
 	};
 };
--- src/routes/(adminLayout)/admin/dashboard/gallery/+page.svelte
+++ src/routes/(adminLayout)/admin/dashboard/gallery/+page.svelte
@@ -1,34 +1,32 @@
+<script>
+	export let data;
+
+	const { galleryImages, galleryVideos } = data;
+</script>
+
 <section class="h-full w-full">
 	<div class="hms-container">
 		<div class="my-16">
 			<h1 class=" text-2xl lg:text-3xl">Update your site gallery here.</h1>
 		</div>
 
-		<div class="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+		<div class="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
 			<a
 				href="/admin/dashboard/gallery/photos"
-				class="flex w-full flex-col justify-between gap-4 rounded bg-base-200 p-4 shadow hover:bg-base-300"
+				class="flex w-full flex-col gap-2 rounded bg-base-200 p-4 shadow hover:bg-base-300"
 			>
-				<h2 class="text-2xl font-bold text-primary">Photos</h2>
+				<h2 class="text-xl font-bold text-primary">Photos</h2>
 
-				<div class="">
-					<p>
-						Total: <b>000</b>
-					</p>
-				</div>
+				<b class=" text-[60px]">{galleryImages?.length}</b>
 			</a>
 
 			<a
 				href="/admin/dashboard/gallery/videos"
-				class="flex w-full flex-col justify-between gap-4 rounded bg-base-200 p-4 shadow hover:bg-base-300"
+				class="flex w-full flex-col gap-2 rounded bg-base-200 p-4 shadow hover:bg-base-300"
 			>
-				<h2 class="text-2xl font-bold text-primary">Videos</h2>
+				<h2 class="text-xl font-bold text-primary">Videos</h2>
 
-				<div class="">
-					<p>
-						Total: <b>000</b>
-					</p>
-				</div>
+				<b class=" text-[60px]">{galleryVideos?.length}</b>
 			</a>
 		</div>
 	</div>
