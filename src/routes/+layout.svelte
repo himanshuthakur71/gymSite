3a2953fdc6ec81fd9721a64b16ced9aca8a764ab
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-17 22:22:10 +0530

    Code Updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+	import 'nprogress/nprogress.css';
 	import '../app.css';
+
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.16,
+		showSpinner: false
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		} else NProgress.done();
+	}
 </script>
 
 <slot></slot>
