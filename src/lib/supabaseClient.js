95489b90bfd69be867860ad0c173303f73cdbec4
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-17 08:32:38 +0530

    supabase keys added to client side

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-	`${import.meta.env.VITE_PUBLIC_SUPABASE_URL}`,
-	`${import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY}`
-);
+const SUPABASE_URL = 'https://itkrfzkrbqgsscxcveae.supabase.co';
+const SUPABASE_ANON_KEY =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0a3JmemtyYnFnc3NjeGN2ZWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg1NDk1NzksImV4cCI6MjAzNDEyNTU3OX0.SW7yIOSh2-00s_NAVjhb5jU9bWDbLpq5Ts-0yOWwZcs';
+
+export const supabase = createClient(`${SUPABASE_URL}`, `${SUPABASE_ANON_KEY}`);
