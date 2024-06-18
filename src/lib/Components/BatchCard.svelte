73b9e8488751d48dac3beb4e3b66bf1a832248c6
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-18 20:28:59 +0530

    Code Updated

---
+++ src/lib/Components/BatchCard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let batch: any;
+</script>
+
+<div class="relative grid w-full grid-cols-1 gap-4 bg-base-200 p-4 shadow-md md:grid-cols-2">
+	<div class=" absolute right-2 top-2 flex gap-4">
+		<a href="/admin/dashboard/batch/{batch?.id}" class="btn btn-primary btn-sm">Edit</a>
+		<button type="button" class="btn btn-error btn-sm">Delete</button>
+	</div>
+	<h2 class="flex flex-col text-lg">
+		<span>Batch Name:</span>
+		<strong>{batch?.batch_name}</strong>
+	</h2>
+
+	<p class="flex flex-col text-lg">
+		<span>Available Limit:</span>
+		<strong>00</strong>
+	</p>
+
+	<p class="flex flex-col text-lg">
+		<span>Total Member in Batch:</span>
+		<strong>00</strong>
+	</p>
+
+	<p class="flex flex-col text-lg">
+		<span>Batch Limit:</span>
+		<strong>{batch?.batch_limit}</strong>
+	</p>
+
+	<p class="flex flex-col text-lg">
+		<span>Batch Timing:</span>
+		<strong>{batch?.batch_open_time} to {batch?.batch_close_time}</strong>
+	</p>
+</div>
--- src/routes/(adminLayout)/admin/dashboard/batch/+page.svelte
+++ src/routes/(adminLayout)/admin/dashboard/batch/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import BatchCard from '$lib/Components/BatchCard.svelte';
 	import { supabase } from '$lib/supabaseClient.js';
 
 	export let data;
@@ -27,38 +28,7 @@
 		<div class="my-16">
 			<div class="grid w-full grid-cols-1 gap-6">
 				{#each gym_batches as batch}
-					<div
-						class="relative grid w-full grid-cols-1 gap-4 bg-base-200 p-4 shadow-md md:grid-cols-2"
-					>
-						<div class=" absolute right-2 top-2 flex gap-4">
-							<a href="/admin/dashboard/batch/{batch?.id}" class="btn btn-primary btn-sm">Edit</a>
-							<button type="button" class="btn btn-error btn-sm">Delete</button>
-						</div>
-						<h2 class="flex flex-col text-lg">
-							<span>Batch Name:</span>
-							<strong>{batch?.batch_name}</strong>
-						</h2>
-
-						<p class="flex flex-col text-lg">
-							<span>Available Limit:</span>
-							<strong>00</strong>
-						</p>
-
-						<p class="flex flex-col text-lg">
-							<span>Total Member in Batch:</span>
-							<strong>00</strong>
-						</p>
-
-						<p class="flex flex-col text-lg">
-							<span>Batch Limit:</span>
-							<strong>{batch?.batch_limit}</strong>
-						</p>
-
-						<p class="flex flex-col text-lg">
-							<span>Batch Timing:</span>
-							<strong>{batch?.batch_open_time} to {batch?.batch_close_time}</strong>
-						</p>
-					</div>
+					<BatchCard {batch} />
 				{/each}
 			</div>
 		</div>
