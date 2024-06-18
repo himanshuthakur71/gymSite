fddad1bc75923aa95d966116370d7a06775f4da5
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-18 15:59:54 +0530

    Code Updated

---
+++ src/routes/(adminLayout)/admin/dashboard/plans/+page.svelte
@@ -0,0 +1,36 @@
+<section class="my-16">
+	<div class="hms-container">
+		<div
+			class="flex items-center gap-4 border-b border-b-gray-600 pb-2 text-2xl font-semibold lg:text-3xl"
+		>
+			<h1>Plans</h1>
+			<span class=" text-gray-600">|</span>
+			<a href="/admin/dashboard/plans/new" class=" link-hover font-[500] text-blue-500">Add</a>
+		</div>
+
+		<div class="my-16">
+			<div class="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
+				<div class="relative grid w-full grid-cols-1 gap-4 bg-base-200 p-4 shadow-md">
+					<div class=" absolute right-2 top-2 flex gap-4">
+						<a href="/admin/dashboard/plans" class="btn btn-primary btn-sm">Edit</a>
+						<button type="button" class="btn btn-error btn-sm">Delete</button>
+					</div>
+					<h2 class="flex flex-col text-lg">
+						<span>Plan Name:</span>
+						<strong>xxxxxxx</strong>
+					</h2>
+
+					<p class="flex flex-col text-lg">
+						<span>Complete Amount:</span>
+						<strong>00</strong>
+					</p>
+
+					<p class="flex flex-col text-lg">
+						<span>Month:</span>
+						<strong>00</strong>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ src/routes/(adminLayout)/admin/dashboard/plans/new/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	let loading = false;
+</script>
+
+<section class="my-16">
+	<div class="hms-container">
+		<div
+			class="flex items-center gap-4 border-b border-b-gray-600 pb-2 text-2xl font-semibold lg:text-3xl"
+		>
+			<h1>Add New Plan</h1>
+		</div>
+
+		<div class="my-16 w-full">
+			<form>
+				<div class="grid w-full grid-cols-1 gap-4 bg-base-300 p-4">
+					<div class="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
+						<label class="form-control w-full">
+							<div class="label">
+								<span class="label-text">Plan Name *</span>
+							</div>
+							<input
+								type="text"
+								placeholder="Type here"
+								class="input input-bordered w-full"
+								required
+							/>
+						</label>
+
+						<label class="form-control w-full">
+							<div class="label">
+								<span class="label-text">Plan Amount *</span>
+							</div>
+							<input
+								type="number"
+								placeholder="Type here"
+								class="input input-bordered w-full"
+								required
+							/>
+						</label>
+					</div>
+
+					<label class="form-control w-full">
+						<div class="label">
+							<span class="label-text">Plan Time (Months) *</span>
+						</div>
+						<select class="select select-bordered" required>
+							<option disabled selected value="">Select</option>
+							{#each Array(24) as _, i}
+								<option value={i + 1}>{i + 1} Month</option>
+							{/each}
+						</select>
+					</label>
+				</div>
+
+				<div class="mt-6 flex justify-between">
+					<button
+						disabled={loading}
+						type="submit"
+						class="btn btn-primary btn-lg btn-block max-w-[140px] text-2xl font-[600]"
+					>
+						Save
+						{#if loading}
+							<span class="loading"></span>
+						{/if}
+					</button>
+
+					<a
+						href="/admin/dashboard/plans"
+						class="btn btn-outline btn-primary btn-lg btn-block max-w-[140px] text-2xl font-[600]"
+					>
+						Cancel
+					</a>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
