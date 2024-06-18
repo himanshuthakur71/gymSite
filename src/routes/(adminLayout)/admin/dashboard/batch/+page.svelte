00c2bbbc295980545469aa6cc44a66bab972a205
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-18 15:51:45 +0530

    Code Updated

---
+++ src/routes/(adminLayout)/admin/dashboard/batch/+page.svelte
@@ -0,0 +1,44 @@
+<section class="my-16">
+	<div class="hms-container">
+		<div
+			class="flex items-center gap-4 border-b border-b-gray-600 pb-2 text-2xl font-semibold lg:text-3xl"
+		>
+			<h1>Batch</h1>
+			<span class=" text-gray-600">|</span>
+			<a href="/admin/dashboard/batch/new" class=" link-hover font-[500] text-blue-500">Add</a>
+		</div>
+
+		<div class="my-16">
+			<div class="relative grid w-full grid-cols-1 gap-4 bg-base-200 p-4 shadow-md md:grid-cols-2">
+				<div class=" absolute right-2 top-2 flex gap-4">
+					<a href="/admin/dashboard/plans" class="btn btn-primary btn-sm">Edit</a>
+					<button type="button" class="btn btn-error btn-sm">Delete</button>
+				</div>
+				<h2 class="flex flex-col text-lg">
+					<span>Batch Name:</span>
+					<strong>xxMorningxx</strong>
+				</h2>
+
+				<p class="flex flex-col text-lg">
+					<span>Available Limit:</span>
+					<strong>00</strong>
+				</p>
+
+				<p class="flex flex-col text-lg">
+					<span>Total Member in Batch:</span>
+					<strong>00</strong>
+				</p>
+
+				<p class="flex flex-col text-lg">
+					<span>Batch Limit:</span>
+					<strong>00</strong>
+				</p>
+
+				<p class="flex flex-col text-lg">
+					<span>Batch Timing:</span>
+					<strong>00:00 XM to 00:00 XM</strong>
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ src/routes/(adminLayout)/admin/dashboard/batch/new/+page.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	let loading = false;
+</script>
+
+<section class="my-16">
+	<div class="hms-container">
+		<div
+			class="flex items-center gap-4 border-b border-b-gray-600 pb-2 text-2xl font-semibold lg:text-3xl"
+		>
+			<h1>Add New Batch</h1>
+		</div>
+
+		<div class="my-16 w-full">
+			<form>
+				<div class="grid w-full grid-cols-1 gap-4 bg-base-300 p-4">
+					<div class="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
+						<label class="form-control w-full">
+							<div class="label">
+								<span class="label-text">Batch Name *</span>
+							</div>
+							<input
+								type="text"
+								placeholder="Type here"
+								class="input input-bordered w-full"
+								required
+							/>
+						</label>
+
+						<label class="form-control w-full">
+							<div class="label">
+								<span class="label-text">Batch Limit *</span>
+							</div>
+							<input
+								type="number"
+								placeholder="Type here"
+								class="input input-bordered w-full"
+								required
+							/>
+						</label>
+
+						<label class="form-control w-full">
+							<div class="label">
+								<span class="label-text">Batch Open Time *</span>
+							</div>
+							<input
+								type="time"
+								placeholder="Type here"
+								class="input input-bordered w-full"
+								required
+							/>
+						</label>
+
+						<label class="form-control w-full">
+							<div class="label">
+								<span class="label-text">Batch Close Time *</span>
+							</div>
+							<input
+								type="time"
+								placeholder="Type here"
+								class="input input-bordered w-full"
+								required
+							/>
+						</label>
+					</div>
+				</div>
+
+				<div class="mt-6 flex justify-between">
+					<button
+						disabled={loading}
+						type="submit"
+						class="btn btn-primary btn-lg btn-block max-w-[140px] text-2xl font-[600]"
+					>
+						Save
+						{#if loading}
+							<span class="loading"></span>
+						{/if}
+					</button>
+
+					<a
+						href="/admin/dashboard/batch"
+						class="btn btn-outline btn-primary btn-lg btn-block max-w-[140px] text-2xl font-[600]"
+					>
+						Cancel
+					</a>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
