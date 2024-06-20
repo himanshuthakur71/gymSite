eb706b092736886c451684b8c7b06c777def190d
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2024-06-20 21:40:39 +0530

    Code Update

--- src/lib/Components/ContactForm.svelte
+++ src/lib/Components/ContactForm.svelte
@@ -1,36 +1,98 @@
-<form>
+<script lang="ts">
+	import emailjs from '@emailjs/browser';
+
+	let formFields = {
+		first_name: '',
+		last_name: '',
+		email: '',
+		phone: '',
+		mesaage: ''
+	};
+
+	const sendEmail = async () => {
+		var templateParams = {
+			from_name: `${formFields?.first_name} ${formFields?.last_name}`,
+			from_mobile: formFields?.phone,
+			from_email: formFields?.email,
+			message: formFields?.mesaage
+		};
+
+		emailjs.init({
+			publicKey: 'fxNNEM5-t8JUTaGVl'
+		});
+
+		emailjs.send('service_yg3x2wb', 'template_d9a0hf7', templateParams).then(
+			(response) => {
+				console.log('SUCCESS!', response.status, response.text);
+			},
+			(error) => {
+				console.log('FAILED...', error);
+			}
+		);
+	};
+</script>
+
+<form on:submit|preventDefault={sendEmail}>
 	<div class="grid w-full grid-cols-1 gap-4">
 		<div class="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
 			<label class="form-control w-full">
 				<div class="label">
 					<span class="label-text font-semibold">First Name *</span>
 				</div>
-				<input type="text" placeholder="First name" class="input input-bordered w-full" required />
+				<input
+					type="text"
+					placeholder="First name"
+					class="input input-bordered w-full"
+					required
+					bind:value={formFields.first_name}
+				/>
 			</label>
 			<label class="form-control w-full">
 				<div class="label">
 					<span class="label-text font-semibold">Last Name *</span>
 				</div>
-				<input type="text" placeholder="Last name" class="input input-bordered w-full" required />
+				<input
+					type="text"
+					placeholder="Last name"
+					class="input input-bordered w-full"
+					required
+					bind:value={formFields.last_name}
+				/>
 			</label>
 			<label class="form-control w-full">
 				<div class="label">
 					<span class="label-text font-semibold">Email *</span>
 				</div>
-				<input type="email" placeholder="Email" class="input input-bordered w-full" required />
+				<input
+					type="email"
+					placeholder="Email"
+					class="input input-bordered w-full"
+					required
+					bind:value={formFields.email}
+				/>
 			</label>
 			<label class="form-control w-full">
 				<div class="label">
 					<span class="label-text font-semibold">Phone *</span>
 				</div>
-				<input type="text" placeholder="Phone" class="input input-bordered w-full" required />
+				<input
+					type="text"
+					placeholder="Phone"
+					class="input input-bordered w-full"
+					required
+					bind:value={formFields.phone}
+				/>
 			</label>
 		</div>
 		<label class="form-control">
 			<div class="label">
 				<span class="label-text font-semibold">Any Other Questions for Us?</span>
 			</div>
-			<textarea class="textarea textarea-bordered h-24" placeholder="Type here..."></textarea>
+			<textarea
+				class="textarea textarea-bordered h-24"
+				placeholder="Type here..."
+				bind:value={formFields.mesaage}
+			></textarea>
 		</label>
 	</div>
 	<div class="mt-12 flex w-full flex-wrap justify-between gap-4">
